344bd42fd56f4e6386f6562d2f639eb8ca946891
AungKyawPhyo1142 <aungkyawphyo22233@gmail.com>
2022-10-03 13:44:56 +0630

    Reusable Components

---
+++ src/views/shopList/AllList.vue
@@ -0,0 +1,20 @@
+<template>
+    <sg-list></sg-list>
+    <about></about>
+</template>
+
+<script>
+    import sgList from "../shopList/TaskList.vue";
+    import about from  "../AboutView.vue";
+
+    export default {
+        name: 'AllList',
+        components: {
+            sgList, about
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/views/shopList/TaskList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="shadow-sm my-3 p-3 text-danger border rounded mx-3" v-for="(item,index) in tasks" :key="index">
+        {{item.name}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TaskList',
+        data () {
+            return {
+                title: 'This is all list page',
+                tasks: [
+                    {name: 'Wash dishes', status: false},
+                    {name: 'Wash clothes', status: false},
+                    {name: 'Buy shirt', status: false},
+                    {name: 'Charge Phone', status: false},
+                    {name: 'Watch netflix', status: false}
+                ]
+            }
+        },
+    }
+</script>
