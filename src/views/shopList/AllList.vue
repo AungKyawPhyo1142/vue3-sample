c973b2ed0c824cb564e63ac4b15b9e2daf7c6016
AungKyawPhyo1142 <aungkyawphyo22233@gmail.com>
2022-10-04 13:18:39 +0630

    Props vs Slots

--- src/views/shopList/AllList.vue
+++ src/views/shopList/AllList.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="">
         <div class="">
+            <SlotList>This is a slot text.</SlotList>
             <sg-list :titleText="Title" :paraText="200"></sg-list>
             <about></about>
         </div>
@@ -10,11 +11,12 @@
 <script>
     import sgList from "../shopList/TaskList.vue";
     import about from  "../AboutView.vue";
+    import slotList from "./slotList.vue";
 
     export default {
         name: 'AllList',
         components: {
-            sgList, about
+            sgList, about, slotList
         }
     }
 </script>
---
+++ src/views/shopList/slotList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot></slot>
+        <h1 class="text-danger"><slot>This is empty value</slot></h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'slotList',
+
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
